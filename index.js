e16f64c056601304e098892d401189b7d3fa79b7
moonway <59399356+moonway-dev@users.noreply.github.com>
2023-11-18 06:00:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.json());
 
-app.get('/api/data', (req, res) => {
-  const data = {
-    message: 'Hello from your API!',
-    timestamp: new Date().toISOString()
-  };
-  res.json(data);
+const createDirectoryIfNotExists = (directory) => {
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory, { recursive: true });
+  }
+};
+
+// Save config JSON to root/APPID/config.json
+app.post('/DeployAppConfig/:appid', (req, res) => {
+  const { appid } = req.params;
+  const { DATA } = req.body;
+  const directoryPath = `./${appid}`;
+  const filePath = `${directoryPath}/config.json`;
+  createDirectoryIfNotExists(directoryPath);
+  const jsonData = JSON.stringify(DATA, null, 2);
+  fs.writeFileSync(filePath, jsonData);
+
+  res.status(200).send('Config saved successfully');
 });
 
-app.listen(process.env.PORT || 3000)
+// Get config JSON using appid
+app.get('/GetAppConfig/:appid', (req, res) => {
+  const { appid } = req.params;
+  const filePath = `./${appid}/config.json`;
+
+  try {
+    // Read config JSON file
+    const data = fs.readFileSync(filePath, 'utf8');
+    const jsonData = JSON.parse(data);
+    res.json(jsonData);
+  } catch (err) {
+    res.status(404).send('Config not found');
+  }
+});
+
+app.all('/', (req, res) => {
+  res.send('NexGuard API!');
+});
+
+app.listen(process.env.PORT || 3000);
