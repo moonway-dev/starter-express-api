4b6a353d83c7e5e94c6808028cf80ceb3e5ec8f1
moonway <59399356+moonway-dev@users.noreply.github.com>
2023-11-18 06:05:59 +0300

    aws

--- index.js
+++ index.js
@@ -1,38 +1,74 @@
 const express = require('express');
-const fs = require('fs');
+const AWS = require('aws-sdk');
 const app = express();
 app.use(express.json());
 
-const createDirectoryIfNotExists = (directory) => {
-  if (!fs.existsSync(directory)) {
-    fs.mkdirSync(directory, { recursive: true });
+const dynamoDB = new AWS.DynamoDB({ region: 'your_region' }); // Replace 'your_region' with your AWS region
+const tableName = 'eager-jade-sweatsuitCyclicDB'; // Replace with your DynamoDB table name
+
+const saveConfigToDynamoDB = async (appid, data) => {
+  const params = {
+    TableName: tableName,
+    Item: {
+      AppID: { S: appid },
+      Data: { S: JSON.stringify(data) },
+    },
+  };
+
+  try {
+    await dynamoDB.putItem(params).promise();
+    return true;
+  } catch (error) {
+    console.error('Unable to save config to DynamoDB', error);
+    return false;
   }
 };
 
-// Save config JSON to root/APPID/config.json
-app.post('/DeployAppConfig/:appid', (req, res) => {
-  const { appid } = req.params;
-  const { DATA } = req.body;
-  const directoryPath = `./${appid}`;
-  const filePath = `${directoryPath}/config.json`;
-  createDirectoryIfNotExists(directoryPath);
-  const jsonData = JSON.stringify(DATA, null, 2);
-  fs.writeFileSync(filePath, jsonData);
-
-  res.status(200).send('Config saved successfully');
-});
-
-// Get config JSON using appid
-app.get('/GetAppConfig/:appid', (req, res) => {
-  const { appid } = req.params;
-  const filePath = `./${appid}/config.json`;
+const getConfigFromDynamoDB = async (appid) => {
+  const params = {
+    TableName: tableName,
+    Key: {
+      AppID: { S: appid },
+    },
+  };
 
   try {
-    // Read config JSON file
-    const data = fs.readFileSync(filePath, 'utf8');
-    const jsonData = JSON.parse(data);
-    res.json(jsonData);
-  } catch (err) {
+    const result = await dynamoDB.getItem(params).promise();
+    if (result.Item && result.Item.Data) {
+      return JSON.parse(result.Item.Data.S);
+    }
+    throw new Error('Config not found');
+  } catch (error) {
+    console.error('Error getting config from DynamoDB', error);
+    throw error;
+  }
+};
+
+// Save config JSON to DynamoDB
+app.post('/DeployAppConfig/:appid', async (req, res) => {
+  const { appid } = req.params;
+  const { DATA } = req.body;
+
+  try {
+    const success = await saveConfigToDynamoDB(appid, DATA);
+    if (success) {
+      res.status(200).send('Config saved successfully');
+    } else {
+      res.status(500).send('Unable to save config');
+    }
+  } catch (error) {
+    res.status(500).send('Error saving config');
+  }
+});
+
+// Get config JSON using appid from DynamoDB
+app.get('/GetAppConfig/:appid', async (req, res) => {
+  const { appid } = req.params;
+
+  try {
+    const configData = await getConfigFromDynamoDB(appid);
+    res.json(configData);
+  } catch (error) {
     res.status(404).send('Config not found');
   }
 });
