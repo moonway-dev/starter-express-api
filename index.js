a545966a99be9dd6677361fd406c0ec424daa399
moonway <59399356+moonway-dev@users.noreply.github.com>
2023-11-18 05:51:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/api/data', (req, res) => {
+  const data = {
+    message: 'Hello from your API!',
+    timestamp: new Date().toISOString()
+  };
+  res.json(data);
+});
+
 app.listen(process.env.PORT || 3000)
